55dc79cafca62bfef3b33dad21eececa1c5168a5
Sachit Adhikari <adhikarisachit@gmail.com>
2018-08-28 19:58:52 +1000

    With API connection; form to enter API details and game info

--- socket.js
+++ socket.js
@@ -2,44 +2,52 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var url = "https://live.test.wh.geniussports.com/v2/basketball/read/36473?ak=2d12fafa5e4b7e1c2f0b102ba13bce19"
+var url = "https://live.test.wh.geniussports.com/v2/basketball/read/36473?ak=2d12fafa5e4b7e1c2f0b102ba13bce19";
 
 var request = require('request');
 console.log('Connecting');
-    var data = [];
+var data = [];
 
 
-
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  // socket.on('chat message', function(msg){
-    request({
-  url: url,
-  agentOptions: {
-    rejectUnauthorized: false
-  },
-    encoding: null
+app.get('/apiinfo', function (req, res) {
+    apiURL = req.query.apiurl;
+    apiKey = req.query.apiKey;
+    matchID = req.query.matchID;
 
-})
-    .on('data', function(chunk) {
-        let result = chunk.toString('utf8');
-            io.emit('chat message', result);
+});
 
-        // console.log(buffer.toString('base64'));
-    }).on('end', function() {
-        //at this point data is an array of Buffers
-        //so Buffer.concat() can make us a new Buffer
-        //of all of them together
-        var buffer = Buffer.concat(data);
-        console.log(buffer.toString('base64'));
+
+io.on('connection', function (socket) {
+    socket.on('connectapi', function (msg) {
+        console.log(msg);
+        request({
+            url: url,
+            agentOptions: {
+                rejectUnauthorized: false
+            },
+            encoding: null
+
+        })
+            .on('data', function (chunk) {
+                let result = chunk.toString('utf8');
+                io.emit('data', result);
+
+                // console.log(buffer.toString('base64'));
+            }).on('end', function () {
+            //at this point data is an array of Buffers
+            //so Buffer.concat() can make us a new Buffer
+            //of all of them together
+            var buffer = Buffer.concat(data);
+            console.log(buffer.toString('base64'));
+        });
     });
-  // });
+    // socket.on('chat message', function(msg){
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
